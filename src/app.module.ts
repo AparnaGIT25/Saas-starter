d129638021f4dac7961d6783d17090ad942809fa
Aparna <raia6254@gmail.com>
2026-05-18 16:22:14 +0530

    changes added

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),  // loads .env everywhere
+    PrismaModule,
+    AuthModule,
+    UsersModule,                                 // registers /auth/register, /auth/login, /auth/me
+  ],
+  providers: [AppService],
+})
+export class AppModule { }
